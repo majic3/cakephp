10b436c7d8da1edfd595be6da9043478bc4a2cda
mark_story <mark@mark-story.com>
2010-07-11 18:38:27 -0400

    Adding test case for find(list) with recursive.

--- cake/tests/cases/libs/model/model_read.test.php
+++ cake/tests/cases/libs/model/model_read.test.php
@@ -6357,6 +6357,18 @@ class ModelReadTest extends BaseModelTest {
 			4 => 'garrett (CakePHP)'
 		);
 		$this->assertEqual($result, $expected);
+
+		$TestModel =& new Article();
+		$TestModel->displayField = 'title';
+		$result = $TestModel->find('list', array(
+			'conditions' => array('User.user' => 'mariano'),
+			'recursive' => 0
+		));
+		$expected = array(
+			1 => 'First Article',
+			3 => 'Third Article'
+		);
+		$this->assertEqual($result, $expected);
 	}
 
 /**
